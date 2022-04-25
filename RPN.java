b46e62d3349f11aad7f2fbb4cc13a9cda5938019
Humberto Lopes <hlfs2@cin.ufpe.br>
2022-04-24 23:04:52 -0300

    Análise Léxica (Scanning) implementada

--- RPN.java
+++ RPN.java
@@ -1,11 +1,22 @@
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import stacker.rpn.lexer.Token;
+import stacker.rpn.lexer.TokenType;
 
 public class RPN {
     public static void main(String[] args) throws IOException {
-        Stack<String> stack = new Stack<String>();
+        
+        Pattern pattern = Pattern.compile("\\D+");
+        Stack<Token> stack = new Stack<Token>();
         String ch;
         int a, b;
+        Token tk;
+        TokenType signal;
+        
+        stack.push(new Token(TokenType.NUM, "000")); //Inicializa a pilha para a condição do while
 
         FileReader fr = null;
         BufferedReader br = null;
@@ -17,26 +28,55 @@ public class RPN {
             System.out.println("File not found");
         }
 
-        while ((ch = br.readLine()) != null){
-            if(!ch.equals("*") && !ch.equals("+") && !ch.equals("-") && !ch.equals("/")){
-                stack.push(ch);
-            } else {
-                a = Integer.parseInt(stack.pop());
-                b = Integer.parseInt(stack.pop());
+        while ((ch = br.readLine()) != null && stack.peek().type != TokenType.EOF){
 
-                if(ch.equals("*")){
-                    stack.push(Integer.toString(a * b));
-                } else if(ch.equals("+")) {
-                    stack.push(Integer.toString(a + b));
-                } else if(ch.equals("-")){
-                    stack.push(Integer.toString(b - a));
-                } else if(ch.equals("/")){
-                    stack.push(Integer.toString(b / a));
+            Matcher matcher = pattern.matcher(ch);
+            boolean matchFound = matcher.find();
+
+            if (ch.equals("*")) {
+                tk = new Token(TokenType.STAR, ch);
+            } else if (ch.equals("+")) {
+                tk = new Token(TokenType.PLUS, ch);
+            } else if (ch.equals("-")) {
+                tk = new Token(TokenType.MINUS, ch);
+            } else if (ch.equals("/")) {
+                tk = new Token(TokenType.SLASH, ch);
+            } else if (matchFound){
+                tk = new Token(TokenType.EOF, ch);
+            } else {
+                tk = new Token(TokenType.NUM, ch);
+            }
+
+            stack.push(tk);
+            
+            System.out.println(tk); //Imprimir a lista de tokens reconhecida
+
+            if (stack.peek().type != TokenType.NUM && stack.peek().type != TokenType.EOF) {
+                Token token = stack.pop();
+                signal = token.type;
+                ch = token.lexeme;
+                
+                a = Integer.parseInt(stack.pop().lexeme);
+                b = Integer.parseInt(stack.pop().lexeme);
+
+                if (signal == TokenType.STAR) {
+                    stack.push(new Token(TokenType.NUM, Integer.toString(a * b)));
+                } else if (signal == TokenType.PLUS) {
+                    stack.push(new Token(TokenType.NUM, Integer.toString(a + b)));
+                } else if (signal == TokenType.MINUS) {
+                    stack.push(new Token(TokenType.NUM, Integer.toString(b - a)));
+                } else if (signal == TokenType.SLASH) {
+                    stack.push(new Token(TokenType.NUM, Integer.toString(b / a)));
                 }
+
             }
         }
 
-        System.out.println(stack.pop());
+        if(stack.peek().type == TokenType.EOF){
+            System.out.println("Error: Unexpected character: " + stack.pop().lexeme);
+        } else {
+            System.out.println("Saida: " + stack.pop().lexeme);
+        }
 
         fr.close();
     }
